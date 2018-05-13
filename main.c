03f2c8572c2bf9de71a7dcd7d74150adf2985e70
Alisson Hayasi <hayasi.ahs@gmail.com>
2018-05-13 15:10:13 -0300

    prog v2

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct{
   int R, G, B;
@@ -134,15 +135,95 @@ void sepia(int rows, int columns, pixel matrix[rows][columns]){
 }
 
 
-int main() {
+int main(){
+  printf("[LINHA DE COMANDO] Escolha os efeitos/filtros a serem aplicados.\n"
+         "[CIN] - Níveis de cinza.\n"
+         "[INV] - Inversão.\n"
+         "[LIM] - Limiar.\n"
+         "[MED] - Suavização.\n"
+         "[CON] - Contraste.\n"
+         "[BRI] - Brilho.\n"
+         "[SEP] - Sepia.\n");
+
+  char choices[7][4] = {"CIN", "INV", "LIM", "MED", "CON", "BRI", "SEP"};
+  char filter_choices[28];
+
+  if(fgets(filter_choices, 28, stdin)==NULL){
+      printf("Algo deu errado!\n");
+      return 0;
+  }
+
+  printf("Deseja inserir valor aos parametros dos filtro?\n"
+         "[1] - Sim.\n"
+         "[0] - Nao.\n");
+
+  int i, cond, parameters[3];
+  scanf("%d", &cond);
+  switch(cond){
+    case 1:
+      printf("[LINHA DE PARAMETROS(OPCIONAL)] Escolha os parâmetros dos filtros.\n"
+             "[LIM, L]: Onde, l 0 a 255 (N) - (Default = 50).\n"
+             "[CON, C]: Onde, c 0 a 255 (R) - (Default = 2).\n"
+             "[BRI, B]: Onde, b -255 a 255 (Z) - (Default = 50).\n"
+             "Os parametros que nao deseja adicionar, coloque o valor 256.\n");
+      for(i=0; i<3; i++)
+        scanf("%d", &parameters[i]);
+    break;
+    case 0:
+      parameters[0] = 50;
+      parameters[1] = 2;
+      parameters[2] = 50;
+    break;
+  }
+
+
+  printf("Insira a matriz\n");
   int L, C;
   scanf("%d %d", &L, &C);
   pixel matrix[L][C];
-  pixel matrix_backup[L][C];
   input_matrix(L, C, matrix);
 
-  printf("Inversao\n");
-  sepia(L, C, matrix);
+  int cont;
+  for(i=0, cont=0; i<7; i++){
+    if(strstr(filter_choices, choices[i])){
+      if (strcmp(choices[i], choices[0]) == 0){
+          printf("Aplicando filtro [CIN] - Niveis de Cinza!\n");
+          grayscale(L, C, matrix);
+      }
+      else if (strcmp(choices[i], choices[1]) == 0){
+          printf("Aplicando filtro [INV] - Inversão!\n");
+          invert_colors(L, C, matrix);
+      }
+      else if (strcmp(choices[i], choices[2]) == 0){
+          printf("Aplicando filtro [LIM] - Limiar com limiar %d!\n", parameters[0]);
+          threshold(L, C, matrix, parameters[0]);
+      }
+      else if (strcmp(choices[i], choices[3]) == 0){
+          printf("Aplicando filtro [MED] - Suavização!\n");
+          brush_smoothing(L, C, matrix);
+      }
+      else if (strcmp(choices[i], choices[4]) == 0){
+          printf("Aplicando filtro [CON] - Contraste com valor de ganho %d!\n", parameters[1]);
+          constrat(L, C, matrix, parameters[1]);
+      }
+      else if (strcmp(choices[i], choices[5]) == 0){
+          int value;
+          if (parameters[2] == 256) value = 2;
+          else value = parameters[2];
+          printf("Aplicando filtro [BRI] - Brilho com valor de ganho %d!\n", parameters[2]);
+          brightness(L, C, matrix, parameters[2]);
+      }
+      else if (strcmp(choices[i], choices[6]) == 0){
+          printf("Aplicando filtro [SEP] - Sépia!\n");
+          brush_smoothing(L, C, matrix);
+      }
+    }
+    else{
+      printf("Algo deu errado! Parece que o método inserido não está disponível! :-(\n");
+      return 0;
+    }
+  }
+
   print_matrix(L, C, matrix);
 
   return 0;
