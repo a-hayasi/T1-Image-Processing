f01cb80c0d9859a36caa744be6c4c62fede1e2da
Alisson Hayasi <hayasi.ahs@gmail.com>
2018-05-15 22:32:43 -0300

    v4

--- main.c
+++ main.c
@@ -82,11 +82,32 @@ void threshold(int rows, int columns, pixel matrix[rows][columns], int l){
 
 void brush_smoothing(int rows, int columns, pixel matrix[rows][columns]){
     int i, j;
+
+    pixel matrix_aux[rows][columns];
+
+    for(i=0; i<rows; i++){
+        for(j=0; j<columns; j++){
+            matrix_aux[i][j].R = matrix[i][j].R;
+            matrix_aux[i][j].G = matrix[i][j].G;
+            matrix_aux[i][j].B = matrix[i][j].B;
+        }
+    }
+
+    //memcpy(matrix_aux, matrix, sizeof(columns * rows * sizeof(int)));
+
     for (i = 1; i < rows-1; i++){
         for (j = 1; j < columns-1; j++){
-            matrix[i][j].R = 1/9*(matrix[i-1][j-1].R + matrix[i][j].R + matrix[i+1][j+1].R);
-            matrix[i][j].G = 1/9*(matrix[i-1][j-1].G + matrix[i][j].G + matrix[i+1][j+1].G);
-            matrix[i][j].B = 1/9*(matrix[i-1][j-1].B + matrix[i][j].B + matrix[i+1][j+1].B);
+            matrix[i][j].R = (matrix_aux[i-1][j-1].R + matrix_aux[i-1][j].R + matrix_aux[i-1][j+1].R + \
+                              matrix_aux[i][j-1].R   + matrix_aux[i][j].R   + matrix_aux[i][j+1].R   + \
+                              matrix_aux[i+1][j-1].R + matrix_aux[i-1][j].R + matrix_aux[i-1][j+1].R)/9;
+
+            matrix[i][j].G = (matrix_aux[i-1][j-1].G + matrix_aux[i-1][j].G + matrix_aux[i-1][j+1].G + \
+                              matrix_aux[i][j-1].G   + matrix_aux[i][j].G   + matrix_aux[i][j+1].G   + \
+                              matrix_aux[i+1][j-1].G + matrix_aux[i-1][j].G + matrix_aux[i-1][j+1].G)/9;
+
+            matrix[i][j].B = (matrix_aux[i-1][j-1].B + matrix_aux[i-1][j].B + matrix_aux[i-1][j+1].B + \
+                              matrix_aux[i][j-1].B   + matrix_aux[i][j].B   + matrix_aux[i][j+1].B   + \
+                              matrix_aux[i+1][j-1].B + matrix_aux[i-1][j].B + matrix_aux[i-1][j+1].B)/9;
         }
     }
     fix_matrix(rows, columns, matrix);
@@ -118,11 +139,15 @@ void brightness(int rows, int columns, pixel matrix[rows][columns], int g){
 
 void sepia(int rows, int columns, pixel matrix[rows][columns]){
     int i, j;
+    int pR_aux, pG_aux, pB_aux;
     for (i = 0; i < rows; i++){
         for (j = 0; j < columns; j++){
-            matrix[i][j].R = 0.393*matrix[i][j].R + 0.769*matrix[i][j].G + 0.189*matrix[i][j].B;
-            matrix[i][j].G = 0.349*matrix[i][j].R + 0.686*matrix[i][j].G + 0.168*matrix[i][j].B;
-            matrix[i][j].B = 0.272*matrix[i][j].R + 0.534*matrix[i][j].G + 0.131*matrix[i][j].B;
+            pR_aux = matrix[i][j].R;
+            pG_aux = matrix[i][j].G;
+            pB_aux = matrix[i][j].B;
+            matrix[i][j].R = 0.393*pR_aux + 0.769*pG_aux + 0.189*pB_aux;
+            matrix[i][j].G = 0.349*pR_aux + 0.686*pG_aux + 0.168*pB_aux;
+            matrix[i][j].B = 0.272*pR_aux + 0.534*pG_aux + 0.131*pB_aux;
         }
     }
     fix_matrix(rows, columns, matrix);
@@ -140,31 +165,41 @@ int main(){
     scanf("%d %d", &L, &C);
     pixel matrix[L][C];
 
-    input_matrix(L, C, matrix);
 
-    if(strcmp(effect_choice, "CIN") == 0)
+    if(strcmp(effect_choice, "CIN") == 0){
+        input_matrix(L, C, matrix);
         grayscale(L, C, matrix);
-    else if(strcmp(effect_choice, "INV") == 0)
+    } //ok
+    else if(strcmp(effect_choice, "INV") == 0){
+        input_matrix(L, C, matrix);
         invert_colors(L, C, matrix);
+    } //ok
     else if(strcmp(effect_choice, "LIM") == 0){
         int l;
         scanf("%d", &l);
+        input_matrix(L, C, matrix);
         threshold(L, C, matrix, l);
     }
-    else if(strcmp(effect_choice, "MED") == 0)
+    else if(strcmp(effect_choice, "MED") == 0){
+        input_matrix(L, C, matrix);
         brush_smoothing(L, C, matrix);
+    }
     else if(strcmp(effect_choice, "CON") == 0){
         int g;
         scanf("%d", &g);
+        input_matrix(L, C, matrix);
         constrat(L, C, matrix, g);
     }
     else if(strcmp(effect_choice, "BRI") == 0){
         int g;
         scanf("%d", &g);
+        input_matrix(L, C, matrix);
         constrat(L, C, matrix, g);
     }
-    else if(strcmp(effect_choice, "SEP") == 0)
+    else if(strcmp(effect_choice, "SEP") == 0){
+        input_matrix(L, C, matrix);
         sepia(L, C, matrix);
+    } //ok
     else
         printf("Error\n");
 
